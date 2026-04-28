99bfda55328bbd9576b15c53ca43d7ad95b2af18
A. Kiselev <a.u.kiselev5665@gmail.com>
2026-04-28 11:06:28 +0300

    feat(backend): добавить content type manager-contact-request

---
+++ apps/backend/src/api/manager-contact-request/controllers/manager-contact-request.ts
@@ -0,0 +1,2 @@
+import { factories } from '@strapi/strapi';
+export default factories.createCoreController('api::manager-contact-request.manager-contact-request');
---
+++ apps/backend/src/api/manager-contact-request/services/manager-contact-request.ts
@@ -0,0 +1,2 @@
+import { factories } from '@strapi/strapi';
+export default factories.createCoreService('api::manager-contact-request.manager-contact-request');
